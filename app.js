6a8a15909073cb99eb1191df81a980aef96ac961
Colaaas <Colas.Gontier_Delaunay.Etu@univ-lemans.fr>
2025-04-03 14:34:18 +0100

    partie 2

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const express = require('express');
+const redis = require('redis');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Bonjour depuis Docker !');
+const redisClient = redis.createClient({
+  url: 'redis://redis:6379',
 });
 
-app.listen(port, () => {
+redisClient.on('error', err => console.log('Erreur Redis', err));
+
+app.get('/', async (req, res) => {
+  try {
+    const visits = await redisClient.incr('visits');
+    res.send(`Bonjour depuis Docker ! Vous êtes le visiteur numéro ${visits}`);
+  } catch (error) {
+    res.status(500).send('Erreur serveur : ' + error.message);
+  }
+});
+
+app.listen(port, async () => {
+  await redisClient.connect();
   console.log(`L'application écoute sur le port ${port}`);
 });
+  
